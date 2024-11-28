304ec8306c6007ec24d0f8e57eaed5a60fd3bbc4
dior <diorkama@gmail.com>
2024-11-28 16:58:10 +0000

    update

--- config/payments.php
+++ config/payments.php
@@ -8,9 +8,9 @@ return [
 
     'wave' => [
         'url' => 'https://api.wave.com/v1/checkout/sessions',
-        'auth_bearer' => 'wave_sn_prod_lcxgroer0xj-zte-QKR5pxFg1YjXxVa5qFtF7kbxP-khESGKlTcUn_cWgqHhZwQvExXVa4J_kqgt-EBb-R3374Ycm7-65jiPXQ',
+        'auth_bearer' => 'wave_sn_prod_xxxxxxxxxxxxxxxxxx',
         'currency' => 'XOF',
-        'webhook_secret' => 'wave_sn_WHS_qhke62y2tc16jmheq9mbdqa644mktmng2eyhzbe857sd2n3tq9g0',
+        'webhook_secret' => 'wave_sn_WHS_xxxxxxxxxxxxxxxxxx',
     ],
 
     'orange-money' => [],
